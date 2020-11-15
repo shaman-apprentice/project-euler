b5e76092b586191b29ba1ead7dd42ca720841917
shaman-apprentice <Torsten.Knauf@gmx.de>
2020-11-15 11:20:07 +0100

    Solve problems 09-10

---
+++ python/09/main.py
@@ -0,0 +1,10 @@
+def isPythagoreanTriplet(a, b, c):
+  return a**2 + b**2 == c**2
+
+targetSum = 1000
+for a in range(1, targetSum):
+  for b in range(a, targetSum):
+    c = targetSum - a - b
+    if isPythagoreanTriplet(a, b, c):
+      print(a * b * c)
+      break
---
+++ python/10/main.py
@@ -0,0 +1,3 @@
+import python.utilities.prime as prime
+
+print(sum(prime.getPrimes(2000000)))
