969209c28679dcd66e6108837c892395eb13f377
shaman-apprentice <Torsten.Knauf@gmx.de>
2020-11-16 21:25:26 +0100

    Solve Problem 10

--- java_/_10/Main.java
+++ java_/_10/Main.java
@@ -1,7 +1,11 @@
 package java_._10;
 
+import java_.utilities.Prime;
+
 public class Main {
   public static void main(String[] args) {
-    System.out.println("");
+    System.out.println(String.format("%.0f", Prime.getPrimes(2000000).stream()
+      .map(Double::valueOf)
+      .reduce(0., (acc, p) -> acc + p)));
   }
 }
