8c2d99f77b294c39666939417fa387921272f7a8
shaman-apprentice <Torsten.Knauf@gmx.de>
2020-11-16 21:06:42 +0100

    Solve Java problem 08 and fix (irrelevant) range error in JS 08

--- java_/_08/Main.java
+++ java_/_08/Main.java
@@ -1,7 +1,27 @@
 package java_._08;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 public class Main {
-  public static void main(String[] args) {
-    System.out.println("");
+  public static void main(String[] args) throws IOException {
+    int digitSize = 13;
+    String numberStr = Files.lines(Paths.get("./files/08.txt"))
+      .collect(Collectors.joining(""));
+    
+    double result = IntStream.range(digitSize, numberStr.length() + 1)
+      .mapToObj(i -> numberStr.substring(i - digitSize, i))
+      .map(s -> Arrays.stream(s.split(""))
+        .map(Double::parseDouble)
+        .reduce(1., (acc, i) -> acc * i)
+      )
+      .max(Comparator.naturalOrder()).get();
+
+    System.out.println(String.format("%.0f\n", result));
   }
 }
--- js/08/main.js
+++ js/08/main.js
@@ -4,7 +4,7 @@ const array = require('../utilities/array');
 
 const numberStr = fs.readFileSync('files/08.txt', 'utf8').split(EOL).join('');
 const digitSize = 13;
-const products = array.filledArray(numberStr.length - 1, digitSize)
+const products = array.filledArray(numberStr.length, digitSize)
   .map(i => numberStr.slice(i - digitSize, i))
   .map(s => s.split('').reduce((acc, c) => acc * parseInt(c), 1));
 
