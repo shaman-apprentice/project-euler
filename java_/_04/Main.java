440e7d08198870df9f1965cfe2c9aff44b39d295
shaman-apprentice <Torsten.Knauf@gmx.de>
2020-11-16 16:30:46 +0100

    Solve Problem 04

--- java_/_04/Main.java
+++ java_/_04/Main.java
@@ -1,7 +1,20 @@
 package java_._04;
 
+import java.util.Comparator;
+import java.util.stream.IntStream;
+
 public class Main {
   public static void main(String[] args) {
-    System.out.println("");
+    System.out.println(IntStream.range(1, 1000)
+      .boxed()
+      .flatMap(i -> IntStream.range(i, 1000).mapToObj(j -> new int[] { i, j }))
+      .map(ij -> ij[0] * ij[1])
+      .filter(product -> {
+        String reversed = new StringBuilder().append(product).reverse().toString();
+        return reversed.equals(String.valueOf(product));
+      })
+      .max(Comparator.naturalOrder())
+      .get()
+    );
   }
 }
