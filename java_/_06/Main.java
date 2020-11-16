4077d4847cdf311061ffc3973ea4a03868d4c5e7
shaman-apprentice <Torsten.Knauf@gmx.de>
2020-11-16 18:47:17 +0100

    Solve problem 06 and 07

--- java_/_06/Main.java
+++ java_/_06/Main.java
@@ -1,7 +1,12 @@
 package java_._06;
 
+import java.util.stream.IntStream;
+
 public class Main {
   public static void main(String[] args) {
-    System.out.println("");
+    double sumOfSquares = IntStream.range(1, 101).asDoubleStream()
+      .reduce(0, (acc, n) -> acc + Math.pow(n, 2));
+    double squareOfSums = Math.pow(100 * 101 / 2, 2);
+    System.out.println(String.format("%.0f\n", squareOfSums - sumOfSquares));
   }
 }
--- java_/_07/Main.java
+++ java_/_07/Main.java
@@ -1,7 +1,14 @@
 package java_._07;
 
+import java.util.stream.Stream;
+
+import java_.utilities.Prime;
+
 public class Main {
   public static void main(String[] args) {
-    System.out.println("");
+    System.out.println(Stream.generate(Prime.generatePrimes()::next)
+      .skip(10000)
+      .findFirst().get()
+    );
   }
 }
