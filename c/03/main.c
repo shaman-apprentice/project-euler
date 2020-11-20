39bb725915d7d2d0e9fd28b9ad1899fd892a01de
shaman-apprentice <Torsten.Knauf@gmx.de>
2020-11-20 19:35:04 +0100

    Solve problem 03

--- c/03/main.c
+++ c/03/main.c
@@ -5,17 +5,6 @@
 #include "../utilities/PrimeFactor.h"
 
 int main() {
-  // struct DPrimeFactorArray pfs = createDPrimeFactorArray(2);
-  // for (int i = 0; i < 3; i++) {
-  //   PrimeFactor pf;
-  //   pf.base = 42;
-  //   pf.exponent = i;
-  //   pfs.push(&pfs, pf);
-  // }
-
-  // printf("size: %d, 3. value: %d ^ %d\n", pfs.size, pfs.array[2].base, pfs.array[2].exponent);
-
-  struct PrimeGenerator pg = createPrimeGenerator(10);
-  for (int i = 0; i < 20; i++)
-    printf("prime: %d, primes.length: %d\n", pg.nextPrime(&pg), pg.primes.length);
+  struct DPrimeFactorArray primeFactorization = getPrimeFactorization(600851475143);
+  printf("%d\n", primeFactorization.array[primeFactorization.length - 1].base);
 }
