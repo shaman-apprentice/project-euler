2cbbff7c73732ad316772011195912b9320e0ae4
shaman-apprentice <Torsten.Knauf@gmx.de>
2020-11-21 21:20:18 +0100

    Solve problem 04

--- c/04/main.c
+++ c/04/main.c
@@ -1,5 +1,24 @@
 #include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+
+bool isPalindromic(int n) {
+  char str[7]; // size 7 cause last char of string is always `\0`
+  sprintf(str, "%d", n);
+  char strReversed[7];
+  strrev(strcpy(strReversed, str)); // note that strrev returns pointer to inplace changed result, therefore copy first
+
+  return strcmp(str, strReversed) == 0;
+} 
 
 int main() {
-  printf("%d", 42);
+  int max = 0;
+  for (int i = 999; i > 100; i--)
+    for (int j = i; j > 100; j--) {
+      int product = i * j;
+      if (product > max && isPalindromic(product))
+        max = product; 
+    }
+
+  printf("%d", max);
 }
