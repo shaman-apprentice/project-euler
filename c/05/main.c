793994a732813c67992f364a1cc524f3f2ad7ab3
shaman-apprentice <Torsten.Knauf@gmx.de>
2020-11-21 21:40:38 +0100

    Solve problem 05 and 06

--- c/05/main.c
+++ c/05/main.c
@@ -1,5 +1,19 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 int main() {
-  printf("%d", 42);
+  int candidate = 2520;
+  bool isCandidateProvenWrong = false;
+  while (!isCandidateProvenWrong) {
+    isCandidateProvenWrong = true;
+    for (int i = 2; i <= 20; i++) {
+      if (candidate % i != 0) {
+        candidate += 1;
+        isCandidateProvenWrong = false;
+        break;
+      }
+    }
+  }
+  
+  printf("%d", candidate);
 }
--- c/06/main.c
+++ c/06/main.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
+#include <math.h>
 
 int main() {
-  printf("%d", 42);
+  double sumOfSquares = 0;
+  for (int i = 1; i <= 100; i++)
+    sumOfSquares += pow(i, 2);
+
+  double squareOfSums = pow(100 * 101 / 2, 2);
+
+  printf("%f", squareOfSums - sumOfSquares);
 }
