41c1a1c0bc4a338f8dc649984cce034c8b3c9b5a
shaman-apprentice <Torsten.Knauf@gmx.de>
2020-11-22 13:57:47 +0100

    Solve problem 09 and 10

--- c/09/main.c
+++ c/09/main.c
@@ -1,5 +1,19 @@
 #include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+bool isPythagoreanTriplet(int a, int b, int c) {
+  return a * a + b * b == c * c;
+}
 
 int main() {
-  printf("%d", 42);
+  int targetSum = 1000;
+  for (int a = 1; a < targetSum; a++)
+    for (int b = a; b < targetSum; b++) {
+      int c = targetSum - a - b;
+      if (isPythagoreanTriplet(a, b, c)) {
+        printf("%d", a * b * c);
+        exit(0);
+      }
+    }
 }
--- c/10/main.c
+++ c/10/main.c
@@ -1,5 +1,16 @@
 #include <stdio.h>
 
+#include "../utilities/prime.h"
+
 int main() {
-  printf("%d", 42);
+  struct PrimeGenerator pg = createPrimeGenerator();
+  
+  long long sum = 0;
+  int prime = pg.nextPrime(&pg);
+  while (prime < 2000000) {
+    sum += prime;
+    prime = pg.nextPrime(&pg);
+  }
+
+  printf("%lld", sum);
 }
