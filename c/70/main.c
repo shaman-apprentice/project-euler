de6bc0e26d0e033d364676e6b5719fb45a4f1efe
shaman-apprentice <Torsten.Knauf@gmx.de>
2020-11-24 23:22:47 +0100

    Solve problem 70

--- c/70/main.c
+++ c/70/main.c
@@ -8,7 +8,7 @@
 int main() {
   clock_t startTime = clock();
 
-  int max = pow(10, 4);
+  int max = pow(10, 7);
   struct PrimeGenerator pg = createPrimeGenerator();
 
   int minN = -1;
@@ -23,6 +23,6 @@ int main() {
   }
 
   double calcTime = (double)(clock() - startTime);
-  printf("%f\n", calcTime / CLOCKS_PER_SEC);
-  printf("n: %d, nDividedThroughPhi: %f", minN, minNDividedThroughPhi);
+  printf("%f\n", calcTime / CLOCKS_PER_SEC); // 2082.124000s ~34min
+  printf("n: %d, nDividedThroughPhi: %f", minN, minNDividedThroughPhi); // n: 8319823, nDividedThroughPhi: 1.000709
 }
