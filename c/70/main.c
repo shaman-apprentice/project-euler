56b34c52a90ff3bd47bfee755af886e00a8e275b
shaman-apprentice <Torsten.Knauf@gmx.de>
2020-11-24 22:34:15 +0100

    Add time measure and little cleanup

--- c/70/main.c
+++ c/70/main.c
@@ -1,28 +1,28 @@
 #include <stdio.h>
 #include <math.h>
+#include <time.h> 
 
 #include "./utilities.h"
 #include "../utilities/prime.h"
 
 int main() {
+  clock_t startTime = clock();
+
   int max = pow(10, 4);
   struct PrimeGenerator pg = createPrimeGenerator();
-
-  // printf("%d", isMutation(1018, 1017));
-  //printf("%d", phi(pg, 1018));
 
   int minN = -1;
   double minNDividedThroughPhi = max;
   for (int n = 2; n < max; n++) {
-    printf("before phi - ");
-    int phiOfN = phi(&pg, n); // 1018 -.-
-    printf("after phi - ");
+    int phiOfN = phi(&pg, n);
     double nDividedThroughPhi = ((double) n) / phiOfN;
-    printf("%d %d %f\n", n, phiOfN, nDividedThroughPhi);
     if (nDividedThroughPhi < minNDividedThroughPhi && isMutation(n, phiOfN)) {
       minN = n;
       minNDividedThroughPhi = nDividedThroughPhi;
     }
   }
+
+  double calcTime = (double)(clock() - startTime);
+  printf("%f\n", calcTime / CLOCKS_PER_SEC);
   printf("n: %d, nDividedThroughPhi: %f", minN, minNDividedThroughPhi);
 }
--- c/readme.md
+++ c/readme.md
@@ -10,12 +10,15 @@ c\xx\main.exe
 # compile static library
 gcc -o c/utilities/dArray.o -c c/utilities/dArray.c
 gcc -o c/utilities/prime.o -c c/utilities/prime.c
+gcc -o c/70/utilities.o -c c/70/utilities.c
 
 # link static library
 gcc c/03/main.c -o c/03/main c/utilities/prime.o c/utilities/dArray.o
 
 # compile and run
 gcc c/03/main.c -o c/03/main c/utilities/dArray.o c/utilities/prime.o && c\03\main.exe
+
+gcc c/70/main.c -o c/70/main c/70/utilities.o c/utilities/dArray.o c/utilities/prime.o && c\70\main.exe
 ```
 
 ### See output of preprocessor
